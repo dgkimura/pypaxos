229475620db8f1c88f54f104edafb89c42da74bd
David Kimura <dgkimura@aol.com>
2015-07-03 23:33:50 -0700

    change package name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name = "pypaxos",
+    name = "paxos",
     version = "0.0.1",
     author = "David Kimura",
     description = ("Python implementation of paxos consensus algorithm."),

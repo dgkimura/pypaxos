d02ceb21416e35ca8b32cec6e88e738d520dd704
David Kimura <dgkimura@aol.com>
2015-07-06 21:05:24 -0700

    update example

--- examples/basic.py
+++ examples/basic.py
@@ -1,8 +1,10 @@
 # basic.py
 import socket
+import time
 
 from paxos.core.node import Node
 from paxos.net.channel import Channel
+from paxos.net.message import Request, Prepare, Promise, Accept, Accepted
 
 
 def main():
@@ -11,8 +13,17 @@ def main():
     channel = Channel(host, replicas)
 
     node = Node(channel)
-    channel.listen()
+    node.start()
 
+    message = Request(host, host)
+    channel.unicast(message)
+    channel.unicast(Request(host, host))
+    channel.unicast(Request(host, host))
+    channel.unicast(Request(host, host))
+    channel.unicast(Request(host, host))
+    channel.unicast(Request(host, host))
+
+    time.sleep(15)
 
 if __name__ == "__main__":
     main()

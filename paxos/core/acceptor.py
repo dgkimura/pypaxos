2e99a3144eca38d8bca5f785fbc025725f7d168b
David Kimura <dgkimura@aol.com>
2015-07-11 12:47:28 -0700

    add paxos protocol documentation strings

--- paxos/core/acceptor.py
+++ paxos/core/acceptor.py
@@ -6,6 +6,15 @@ from paxos.net.message import Prepare, Promise, Accept, Nack, Accepted
 class Acceptor(Role):
     @Role.receive.register(Prepare)
     def _(self, message, channel, create_reply=Promise.create):
+        """Promise Phase.
+
+        If an acceptor receives a prepare request with number n greater than
+        that of any prepare request to which it has already responded then it
+        responds to the request with a promise not to accept any more
+        proposals numbered less than n and with the highest-numbered proposal
+        (if any) that it has accepted.
+
+        """
         print("RECEIVED message {0}".format(message))
         if message.proposal.number >= self.last_proposal.number:
             reply = create_reply(sender=message.receiver,
@@ -19,6 +28,13 @@ class Acceptor(Role):
 
     @Role.receive.register(Accept)
     def _(self, message, channel, create_reply=Accepted.create):
+        """Accepted Phase.
+
+        If an acceptor receives an accept request for a proposal numbered n,
+        it accepts the proposal unless it has already responded to a prepare
+        request having a number greater than n.
+
+        """
         print("RECEIVED message {0}".format(message))
         reply = create_reply(sender=message.receiver)
         channel.broadcast(reply)
--- paxos/core/proposer.py
+++ paxos/core/proposer.py
@@ -6,12 +6,27 @@ from paxos.net.message import Request, Prepare, Promise, Accept, Accepted
 class Proposer(Role):
     @Role.receive.register(Request)
     def _(self, message, channel, create_reply=Prepare.create):
+        """Prepare Phase.
+
+        A proposer selects a proposal number n and sends a prepare request
+        with number n to a majority of acceptors.
+
+        """
         print("RECEIVED message {0}".format(message))
         reply = create_reply(sender=message.receiver)
         channel.broadcast(reply)
 
     @Role.receive.register(Promise)
     def _(self, message, channel, create_reply=Accept.create):
+        """Accept Phase.
+
+        If the proposer receives a response to its prepare requests (numbered
+        n) from a majority of acceptors, then it sends an accept request to
+        each of those acceptors for a proposal numbered n with a value v, where
+        v is the value of the highest-numbered proposal among the responses, or
+        is any value if the responses reported no proposals.
+
+        """
         print("RECEIVED message {0}".format(message))
         reply = create_reply(sender=message.receiver)
         channel.broadcast(reply)

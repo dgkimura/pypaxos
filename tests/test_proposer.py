f62eaa3f74c06c8f478fa2f85493b0ff8bed4665
David Kimura <dgkimura@aol.com>
2015-07-11 13:12:26 -0700

    add unittest for proposal incrementing logic

--- tests/test_proposer.py
+++ tests/test_proposer.py
@@ -6,13 +6,26 @@ from paxos.net.message import Request, Prepare, Promise, Accept, Accepted
 
 
 class TestProposer(TestCase):
-    def test_receive_request(self):
+    def test_receive_request_sends_initial_proposal(self):
         channel = HistoryChannel()
         role = Proposer()
 
         role.receive(Request.create(), channel)
 
-        self.assertTrue(type(channel.broadcast_messages[0]) is Prepare)
+        sent_message = channel.broadcast_messages[-1]
+        self.assertTrue(type(sent_message) is Prepare)
+        self.assertEqual(sent_message.proposal.number, 1)
+
+    def test_receive_request_sends_subsequent_propsal(self):
+        channel = HistoryChannel()
+        role = Proposer()
+
+        role.receive(Request.create(), channel)
+        role.receive(Request.create(), channel)
+        role.receive(Request.create(), channel)
+
+        sent_message = channel.broadcast_messages[-1]
+        self.assertEqual(sent_message.proposal.number, 3)
 
     def test_receive_promise(self):
         channel = HistoryChannel()

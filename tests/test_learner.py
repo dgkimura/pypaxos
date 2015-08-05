0316d77aa68a2e3a6ed982d4f2b1ef065560bd82
David Kimura <dgkimura@aol.com>
2015-08-04 19:30:04 -0700

    add unittest for duplicate learned proposal messages

--- tests/test_learner.py
+++ tests/test_learner.py
@@ -3,16 +3,29 @@ from unittest import TestCase, main
 from paxos.core.learner import Learner
 from paxos.net.history_channel import HistoryChannel
 from paxos.net.message import Accepted, Response
+from paxos.net.proposal import Proposal
 
 
 class TestLearner(TestCase):
     def test_receive_accepted(self):
-        channel = HistoryChannel()
+        channel = HistoryChannel(replicas=['A', 'B', 'C'])
         role = Learner()
 
-        role.receive(Accepted.create(), channel)
+        role.receive(Accepted.create(proposal=Proposal('A', 1), sender='A'), channel)
+        role.receive(Accepted.create(proposal=Proposal('A', 1), sender='B'), channel)
+        role.receive(Accepted.create(proposal=Proposal('A', 1), sender='C'), channel)
 
         self.assertTrue(type(channel.unicast_messages[0]) is Response)
+
+    def test_receive_duplicate_accepted_proposals(self):
+        channel = HistoryChannel(replicas=['A', 'B', 'C'])
+        role = Learner()
+
+        role.receive(Accepted.create(proposal=Proposal('A', 1), sender='A'), channel)
+        role.receive(Accepted.create(proposal=Proposal('A', 1), sender='A'), channel)
+        role.receive(Accepted.create(proposal=Proposal('A', 1), sender='A'), channel)
+
+        self.assertFalse(channel.unicast_messages)
 
 
 if __name__ == "__main__":
